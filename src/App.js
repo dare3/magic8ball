7ea31b739210630745bb2651d37620503cafb81a
Dare3 <enitan_segun@yahoo.co.uk>
2024-09-16 13:58:44 -0400

    import components such as EightBall.js EightBall.css and ColorBoxes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import EightBall from './EightBall';
+import ColorBoxes from './ColorBoxes';
+import './EightBall.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Magic Eight Ball</h1>
+      <EightBall />
+      <h1>Color Boxes</h1>
+      <ColorBoxes />
     </div>
   );
-}
+};
 
 export default App;
